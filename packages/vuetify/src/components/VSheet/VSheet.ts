bb41e99fc6ef8e82c97b18b2652c6ad64883b876
John Leider <john.j.leider@gmail.com>
2021-01-04 11:08:06 -0600

    chore: init commit

--- packages/vuetify/src/components/VSheet/VSheet.ts
+++ packages/vuetify/src/components/VSheet/VSheet.ts
@@ -4,67 +4,87 @@
 // Styles
 import './VSheet.sass'
 
-// Mixins
-import BindsAttrs from '../../mixins/binds-attrs'
-import Colorable from '../../mixins/colorable'
-import Elevatable from '../../mixins/elevatable'
-import Measurable from '../../mixins/measurable'
-import Roundable from '../../mixins/roundable'
-import Themeable from '../../mixins/themeable'
+// Utilities
+import { defineComponent, computed, h, mergeProps } from 'vue'
+import { makeBorderRadiusProps, useBorderRadiusClasses } from '../../composables/border-radius'
+import { dimensionsFactory } from '../../composables/dimensions'
+import { makeElevationProps, useElevationClasses } from '../../composables/elevation'
+import { makeTagProps } from '../../composables/tag'
+import { useTheme } from '../../composables/theme'
+import makeProps from '../../util/makeProps'
 
-// Helpers
-import mixins from '../../util/mixins'
-
-// Types
-import { VNode } from 'vue'
+const { makeDimensionsProps, useDimensions } = dimensionsFactory('width', 'height')
 
 /* @vue/component */
-export default mixins(
-  BindsAttrs,
-  Colorable,
-  Elevatable,
-  Measurable,
-  Roundable,
-  Themeable
-).extend({
-  name: 'v-sheet',
+export default defineComponent({
+  name: 'VSheet',
 
-  props: {
-    outlined: Boolean,
-    shaped: Boolean,
-    tag: {
-      type: String,
-      default: 'div',
-    },
-  },
+  props: makeProps({
+    ...makeBorderRadiusProps(),
+    ...makeDimensionsProps(),
+    ...makeElevationProps(),
+    ...makeTagProps(),
+  }),
 
-  computed: {
-    classes (): object {
-      return {
-        'v-sheet': true,
-        'v-sheet--outlined': this.outlined,
-        'v-sheet--shaped': this.shaped,
-        ...this.themeClasses,
-        ...this.elevationClasses,
-        ...this.roundedClasses,
-      }
-    },
-    styles (): object {
-      return this.measurableStyles
-    },
-  },
+  setup (props, { attrs, slots }) {
+    const { borderRadiusClasses } = useBorderRadiusClasses(props)
+    const { dimensionStyles } = useDimensions(props)
+    const { elevationClasses } = useElevationClasses(props)
+    const theme = useTheme()
 
-  render (h): VNode {
-    const data = {
-      class: this.classes,
-      style: this.styles,
-      on: this.listeners$,
-    }
+    console.log(theme)
+    console.log(dimensionStyles.value)
+    console.log(elevationClasses.value)
 
-    return h(
-      this.tag,
-      this.setBackgroundColor(this.color, data),
-      this.$slots.default
+    return () => (
+      h(props.tag, {
+        ...attrs,
+        class: [
+          'v-sheet',
+          borderRadiusClasses.value,
+          elevationClasses.value,
+        ],
+        style: [dimensionStyles.value]
+      }, slots)
     )
-  },
+  }
+
+  // props: {
+  //   outlined: Boolean,
+  //   shaped: Boolean,
+  //   tag: {
+  //     type: String,
+  //     default: 'div',
+  //   },
+  // },
+
+  // computed: {
+  //   classes (): object {
+  //     return {
+  //       'v-sheet': true,
+  //       'v-sheet--outlined': this.outlined,
+  //       'v-sheet--shaped': this.shaped,
+  //       ...this.themeClasses,
+  //       ...this.elevationClasses,
+  //       ...this.roundedClasses,
+  //     }
+  //   },
+  //   styles (): object {
+  //     return this.measurableStyles
+  //   },
+  // },
+
+  // render (h): VNode {
+  //   const data = {
+  //     class: this.classes,
+  //     style: this.styles,
+  //     on: this.listeners$,
+  //   }
+
+  //   return h(
+  //     this.tag,
+  //     this.setBackgroundColor(this.color, data),
+  //     this.$slots.default
+  //   )
+  // },
 })
